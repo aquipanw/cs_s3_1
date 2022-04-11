ca965cc4466904949a2d136fd1732b15517deff1
Akihiko Iimura <aiimura@paloaltonetworks.com>
2022-04-11 20:05:38 +0900

    change acl

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "log_bucket" {
     bucket = "my-tf-log-bucket"
-    acl = "log-delivery-write"
+    # acl = "log-delivery-write"
+    acl = "public-read"
     tags = {
         region = "us-east-2"
         Demo = "log-delivery-write"
